6f72811ef1317ccbc89ff8b5a18f247a05464967
Oleksii <alexbilous9@gmail.com>
2020-06-19 17:10:58 +0300

    Initial commit

---
+++ src/main/java/com/example/BurgerOnlineExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerOnlineExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurgerOnlineExampleApplication.class, args);
+    }
+
+}
